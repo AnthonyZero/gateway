fe6a8f6a47ae1df75ed0f7d71a889d417b411adb
anthonyzero <736252868@qq.com>
2020-11-09 21:56:44 +0800

    commit

--- controller/service.go
+++ controller/service.go
@@ -1,6 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/anthonyzero/gateway/dao"
+	"github.com/anthonyzero/gateway/dto"
+	"github.com/anthonyzero/gateway/middleware"
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/gin"
+)
 
 type ServiceController struct{}
 
@@ -16,10 +22,46 @@ func ServiceRegister(group *gin.RouterGroup) {
 // @ID /service/service_list
 // @Accept  json
 // @Produce  json
-// @Param info query string false "关键词"
+// @Param keyword query string false "关键词"
 // @Param page_size query int true "每页个数"
 // @Param page_no query int true "当前页数"
 // @Success 200 {object} middleware.Response{data=dto.ServiceListOutput} "success"
 // @Router /service/service_list [get]
 func (service *ServiceController) ServiceList(c *gin.Context) {
+	params := &dto.ServiceListInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, middleware.ParamCheckErrorCode, err)
+		return
+	}
+
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, middleware.InternalErrorCode, err)
+		return
+	}
+
+	//从db中分页读取基本信息
+	serviceInfo := &dao.ServiceInfo{}
+	list, total, err := serviceInfo.PageList(c, tx, params)
+	if err != nil {
+		middleware.ResponseError(c, middleware.BusinessErrorCode, err)
+		return
+	}
+
+	///格式化输出
+	outList := []dto.ServiceListItemOutput{}
+	for _, item := range list {
+		outItem := dto.ServiceListItemOutput{
+			ID:          item.ID,
+			LoadType:    item.LoadType,
+			ServiceName: item.ServiceName,
+			ServiceDesc: item.ServiceDesc,
+		}
+		outList = append(outList, outItem)
+	}
+	out := &dto.ServiceListOutput{
+		Total: total,
+		List:  outList,
+	}
+	middleware.ResponseSuccess(c, out)
 }
