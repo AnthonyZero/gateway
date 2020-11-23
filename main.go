a686213a9e84a11b05093934fac6ef8b2efbbced
anthonyzero <736252868@qq.com>
2020-11-23 20:53:26 +0800

    添加启动参数

--- main.go
+++ main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
-	"github.com/anthonyzero/gateway/router"
-	"github.com/e421083458/golang_common/lib"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/anthonyzero/gateway/router"
+	"github.com/e421083458/golang_common/lib"
 )
 
-func main()  {
-	lib.InitModule("./conf/dev/",[]string{"base","mysql","redis",})
-	defer lib.Destroy()
-	router.HttpServerRun()
+//endpoint:  dashboard ->后台管理接口  server->代理服务器
+//config:  ./conf/prod/ 对应配置文件夹
 
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+//终端输入参数
+var (
+	endpoint = flag.String("endpoint", "", "input endpoint dashboard or server")
+	config   = flag.String("config", "", "input config file like ./conf/dev/")
+)
 
-	router.HttpServerStop()
+func main() {
+	flag.Parse()
+	if *endpoint == "" {
+		//自动打印错误提示
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *config == "" {
+		//自动打印错误提示
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	//要么运行后台接口部分 要么运行代理服务器
+	if *endpoint == "dashboard" {
+		lib.InitModule(*config, []string{"base", "mysql", "redis"})
+		defer lib.Destroy()
+		router.HttpServerRun()
+
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		router.HttpServerStop()
+	} else {
+		lib.InitModule(*config, []string{"base", "mysql", "redis"})
+		defer lib.Destroy()
+
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+	}
 }
