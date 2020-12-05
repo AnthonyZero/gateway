e97db4861078545ecf669f7ac9a6344485626136
anthonyzero <736252868@qq.com>
2020-12-05 16:24:38 +0800

    header transfor

--- http_proxy_router/route.go
+++ http_proxy_router/route.go
@@ -15,7 +15,9 @@ func InitRouter(middlewares ...gin.HandlerFunc) *gin.Engine {
 			"message": "pong",
 		})
 	})
-	router.Use(http_proxy_middleware.HTTPAccessModeMiddleware(),
+	router.Use(
+		http_proxy_middleware.HTTPAccessModeMiddleware(),
+		http_proxy_middleware.HTTPHeaderTransferMiddleware(),
 		http_proxy_middleware.HTTPReverseProxyMiddleware())
 	return router
 }
